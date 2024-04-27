88c76166176151609ba8d7403da61a8bf2dc09d2
shebll <shebblloll@gmail.com>
2024-04-27 22:06:09 +0300

    fix hero responsive

--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { LazyMotion, domAnimation, m } from "framer-motion"
-import Image from "next/image"
-import { useState, useEffect, useContext } from "react"
-import Link from "next/link"
+"use client";
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import Image from "next/image";
+import { useState, useEffect, useContext } from "react";
+import Link from "next/link";
 
 function Hero() {
   return (
     <LazyMotion features={domAnimation}>
       <div className="hero ">
-        <div className="container mx-auto min-h-[80vh] px-4  ">
-          <div className="flex flex-col gap-10 items-center pt-50">
+        <div className="container mx-auto min-h-[76vh] px-4">
+          <div className="flex flex-col items-center gap-10 pt-50">
             <m.div
               initial={{ y: 100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
@@ -19,19 +19,26 @@ function Hero() {
                 stiffness: 50,
                 delay: 0.7,
               }}
-              className="shh w-full  justify-center gap-20   items-center"
+              className="items-center justify-center w-full gap-20"
             >
-              <Image
-                src="https://drive.google.com/uc?export=view&id=1E9n67LzNYQSWsMXSFpRXjhch4u_sRx9R"
-                width={600}
-                style={{
-                  paddingTop: "15%",
-                }}
-                height={600}
-                alt="hero section image"
-                loading="lazy"
-                className="object-contain"
-              />
+              <div className="flex items-center justify-between pt-20 lg:pt-0">
+                <Image
+                  src="https://drive.google.com/uc?export=view&id=1E9n67LzNYQSWsMXSFpRXjhch4u_sRx9R"
+                  width={500}
+                  height={500}
+                  alt="hero section image"
+                  loading="lazy"
+                  className="object-contain"
+                />
+                <Image
+                  className="hidden 2xl:block "
+                  src="https://drive.google.com/uc?export=view&id=1JG2BHRs9A39_JZWQg61IgiRkboAbXCzO"
+                  width={500}
+                  height={400}
+                  alt="hero section image"
+                  loading="lazy"
+                />
+              </div>
               <m.p
                 initial={{ y: 100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
@@ -50,7 +57,7 @@ function Hero() {
                 ensuring customer satisfaction.
               </m.p>
               <Link href="https://drive.google.com/file/d/1eKxEL6Y1SC_rZ3OT27mTwjjsYl3RFkTH/view?usp=drivesdk">
-                <m.button style={{}} className="bhero mb-2 ">
+                <m.button style={{}} className="mb-2 bhero ">
                   SHADEL CATALOGUE{" "}
                 </m.button>
               </Link>
@@ -63,20 +70,11 @@ function Hero() {
                 Download Brochure
               </m.button>
             </Link> */}
-            <Image
-              className="hidden 2xl:block "
-              src="https://drive.google.com/uc?export=view&id=1JG2BHRs9A39_JZWQg61IgiRkboAbXCzO"
-              width={600}
-              height={400}
-              style={{ position: "absolute", right: 300, top: "25%" }}
-              alt="hero section image"
-              loading="lazy"
-            />
           </div>
         </div>
       </div>
     </LazyMotion>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
