a1184e630bdb619507a5fda356776a23d08c6fa1
Hossam-sultan <hossamsultan11@gmail.com>
2024-12-01 01:04:16 +0200

    Update

--- app/(site)/components/Hero.tsx
+++ app/(site)/components/Hero.tsx
@@ -69,7 +69,7 @@ function Hero() {
                 <br /> Our commitment to quality and innovation drives us,
                 ensuring customer satisfaction.
               </m.p>
-              <Link href="https://drive.google.com/file/d/1ntTzGQniFzETshzyk4d4bD8k6Ga1YiPi/view?usp=drive_link">
+              <Link href="https://drive.google.com/file/d/1ntTzGQniFzETshzyk4d4bD8k6Ga1YiPi/preview">
                 <m.button style={{}} className="mb-2 bhero bold-text ">
                   SHADEL CATALOGUE{" "}
                 </m.button>
